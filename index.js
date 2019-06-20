e458f24c9d9ac7a7cde493a7cd703fd66c79295f
Winston Tokuhisa <tokuhisawinston@outlook.com>
2019-06-20 04:20:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,11 +10,40 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+  // write your code here
+  var cartItem = {};
+  cartItem.itemName = item;
+  cartItem.itemPrice = Math.ceil(Math.random() * 100);
+  cart.push(cartItem);
+
+  return `${cartItem.itemName} has been added to your cart.`;
 }
 
+// The viewCart() function does not accept any arguments. It
+// should loop over every item in your cart, returning the
+// contents as one long, coherent statement in this format: In
+// your cart, you have bananas at $17, pancake batter at $5,
+// and eggs at $49.
 function viewCart() {
   // write your code here
+  // If the cart is empty, the function should instead return
+  // Your shopping cart is empty.
+  if (cart.length < 1) { return 'Your shopping cart is empty.'; }
+// Note: Pay close attention to the syntax above. The returned
+// statement should be a single sentence that begins with In your
+// cart, you have, terminates in a period, and can assume the
+// following shapes according to how many items the cart contains:
+  // 1 item — In your cart, you have bananas at $17.
+  if (cart.length == 1) {
+    return `In your cart, you have ${cart[0].itemName} at $${cart[0].itemPrice}.`;
+  }
+  // 2 items — In your cart, you have bananas at $17, and pancake
+  // batter at $5.
+  if (cart.length == 2) {
+    return `In your cart, you have ${cart[0].itemName} at $${cart[0].itemPrice}, and ${cart[1].itemName} at $${cart[1].itemPrice}.`;
+  }
+  // 3+ items — In your cart, you have bananas at $17, pancake
+  // batter at $5, and eggs at $49.
 }
 
 function total() {
