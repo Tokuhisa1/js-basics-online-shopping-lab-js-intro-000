32c2c5653d28b019a57266b76bf8d31788e2c474
Winston Tokuhisa <tokuhisawinston@outlook.com>
2019-06-20 04:40:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function setCart(c) {
 
 function addToCart(item) {
   var cartItem = {};
-  
+
   cartItem.itemName = item;
   cartItem.itemPrice = Math.ceil(Math.random() * 100);
   cart.push(cartItem);
@@ -22,10 +22,10 @@ function addToCart(item) {
 function viewCart() {
   if (cart.length < 1) { return 'Your shopping cart is empty.'; }
   var view = 'In your cart, you have ';
-  
+
   for (var i = 0; i < cart.length; i = i + 1) {
     view = view + `${cart[i].itemName} at $${cart[i].itemPrice}`;
-    
+
     if (i < cart.length - 1) { view = view + ', '; }
     if (i == cart.length - 2) { view = view + 'and '; }
   }
