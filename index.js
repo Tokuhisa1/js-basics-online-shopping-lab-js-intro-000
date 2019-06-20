05fa38002dcc42eda539bba04750645af64d7146
Winston Tokuhisa <tokuhisawinston@outlook.com>
2019-06-20 04:40:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,8 +10,8 @@ function setCart(c) {
 }
 
 function addToCart(item) {
-  // write your code here
   var cartItem = {};
+  
   cartItem.itemName = item;
   cartItem.itemPrice = Math.ceil(Math.random() * 100);
   cart.push(cartItem);
@@ -19,20 +19,17 @@ function addToCart(item) {
   return `${cartItem.itemName} has been added to your cart.`;
 }
 
-// The viewCart() function does not accept any arguments. It
-// should loop over every item in your cart, returning the
-// contents as one long, coherent statement in this format: In
-// your cart, you have bananas at $17, pancake batter at $5,
-// and eggs at $49.
 function viewCart() {
-  // write your code here
   if (cart.length < 1) { return 'Your shopping cart is empty.'; }
   var view = 'In your cart, you have ';
+  
   for (var i = 0; i < cart.length; i = i + 1) {
     view = view + `${cart[i].itemName} at $${cart[i].itemPrice}`;
+    
     if (i < cart.length - 1) { view = view + ', '; }
     if (i == cart.length - 2) { view = view + 'and '; }
   }
+  
   return view + '.';
 }
 
